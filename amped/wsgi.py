a8b533e9a4b5b664b4540647ee9f6602d236f8b0
Julien N <nguyenduy.j@gmail.com>
2020-02-09 17:43:41 +0900

    wsgi

--- amped/wsgi.py
+++ amped/wsgi.py
@@ -7,10 +7,19 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
-import os
-
+# import os
+#
 from django.core.wsgi import get_wsgi_application
+#
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'amped.settings')
+#
+# application = get_wsgi_application()
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'amped.settings')
 
+import os
+import sys
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 application = get_wsgi_application()
