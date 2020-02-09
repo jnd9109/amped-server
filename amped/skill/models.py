3ffd77d9f546b841b2ecb7638acbdec73b8c39ac
Julien N <nguyenduy.j@gmail.com>
2020-02-09 17:09:56 +0900

    add requirements

--- amped/skill/models.py
+++ amped/skill/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Skill(models.Model):
     name = models.CharField(max_length=128)
     description = models.TextField(blank=True, null=True)
-    sub_skills = models.ManyToManyField('self', related_name='parent_skill', blank=True)
+    sub_skills = models.ManyToManyField('self', related_name='parent_skill', blank=True, symmetrical=False)
 
     def __str__(self):
         return f'[{self.id}] {self.name}'
--- amped/skill/serializers.py
+++ amped/skill/serializers.py
@@ -2,14 +2,14 @@ from rest_framework.serializers import ModelSerializer, Serializer
 from skill.models import Skill
 
 
-class RecursiveField(Serializer):
-    def to_representation(self, value):
-        serializer = self.parent.parent.__class__(value, context=self.context)
-        return serializer.data
+class SubCategorySerializer(ModelSerializer):
+    class Meta:
+        model = Skill
+        fields = ('id', 'name', 'description')
 
 
 class SkillSerializer(ModelSerializer):
-    sub_skills = RecursiveField(many=True)
+    sub_skills = SubCategorySerializer(many=True)
 
     class Meta:
         model = Skill
