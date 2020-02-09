860d1dd02ba89176c03aeb5f5311153e09190d55
Julien N <nguyenduy.j@gmail.com>
2020-02-09 18:04:53 +0900

    wsgi

--- amped/amped/wsgi.py
+++ amped/amped/wsgi.py
@@ -21,5 +21,6 @@ import sys
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'amped.settings'
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "amped.settings")
 application = get_wsgi_application()
