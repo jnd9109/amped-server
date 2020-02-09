9ca719ce5507a353f934d213113a38e681100372
Julien N <nguyenduy.j@gmail.com>
2020-02-09 19:15:06 +0900

    wsgi

--- amped/amped/wsgi.py
+++ amped/amped/wsgi.py
@@ -16,15 +16,8 @@ from django.core.wsgi import get_wsgi_application
 # application = get_wsgi_application()
 
 
-# import os
-#
-# os.environ['DJANGO_SETTINGS_MODULE'] = 'amped.settings'
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "amped.settings")
-# application = get_wsgi_application()
-
-
 import os
-from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'amped.settings')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'amped.settings'
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "amped.settings")
 application = get_wsgi_application()
